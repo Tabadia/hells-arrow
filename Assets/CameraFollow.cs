5f75b3cbaeaeb8bd6b222bcbd80d10b797c91b6e
Thalen <tabadia7574@student.pps.net>
2023-01-21 00:10:11 -0800

    Made it so camera is attached to player
    Small update but I was bored, basically u can move player around without having to drag camera with it, and the camera doesnt rotate anywhere because of the code I added.

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -6,12 +6,15 @@ public class CameraFollow : MonoBehaviour
     public GameObject player;
     private Vector3 offset;
     private Vector3 newtrans;
+    private Quaternion cameraRotation;
 
     void Start ()
     {
         offset.x = transform.position.x - player.transform.position.x;
         offset.z = transform.position.z - player.transform.position.z;
         newtrans=transform.position;
+        // Get original rotation
+        cameraRotation = this.transform.rotation;
 
     }
     void LateUpdate ()
@@ -19,6 +22,8 @@ public class CameraFollow : MonoBehaviour
         newtrans.x= player.transform.position.x + offset.x;
         newtrans.z= player.transform.position.z + offset.z;
         transform.position = newtrans;
+        // Keep rotation at original rotation so it doesnt follow where the player obj rotates
+        transform.rotation = cameraRotation;
     }
 
 }
