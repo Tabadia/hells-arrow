52885cf29c540247f34e757fa7c93c2495d69af2
aila mengden-koon <akoon6599@student.pps.net>
2023-02-13 12:57:14 -0800

    i really don't understand what git does sometimes. fixing camera changes that magically didnt get included when I pulled

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,23 +9,30 @@ public class CameraController : MonoBehaviour
     [SerializeField] Camera mainCam;
     [SerializeField] Camera secondaryCam;
 
+    public ShootingScript shootingScript;
+
+    void Start() {
+        shootingScript = player.GetComponent<ShootingScript>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         // If the raycast doesn't hit, just focus the camera on the player
         Vector3 zoomPoint = player.position;
-
-        // Sending a ray from camera to mouse position
-        RaycastHit hit;
-        if (Physics.Raycast(secondaryCam.ScreenPointToRay(Input.mousePosition), out hit, 100))
-        {
-            zoomPoint = hit.point;
+        if (shootingScript.isCharging) {
+            // Sending a ray from camera to mouse position
+            RaycastHit hit;
+            if (Physics.Raycast(secondaryCam.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                zoomPoint = hit.point;
+            }
         }
-
         // Getting the average between the focus point and the player position so that the player doesn't go out of view
         Vector3 avgPosition = (player.position + zoomPoint) / 2;
 
         // Setting the camera's position
         mainCam.transform.position = Vector3.Lerp(transform.position, new Vector3(avgPosition.x + 10, transform.position.y, avgPosition.z + 10), Time.deltaTime * speed);
+        
     }
 }
