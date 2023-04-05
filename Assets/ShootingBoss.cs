4d392a116568cc96d862e2e589d1698f1c822b11
plubos <parkerwhall@gmail.com>
2023-04-04 22:29:51 -0700

    ice cave kinda brokey :( need fix

---
+++ Assets/ShootingBoss.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    This is really a bullet hell boss
+    1) Shoots projectiles out from its body in perfect spacing
+    2) Spawns a big icicle from underneath, hitting any player trying to get a hit
+    3) Shoots homing projectiles
+    4) 
+ */
+public class ShootingBoss : MonoBehaviour
+{
+    [SerializeField] private GameObject bigIcicle;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
