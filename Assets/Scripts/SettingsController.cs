ec4103f8133e86912f7df46c97006d960fdc8439
ThalenAbadia <tabadia7574@student.pps.net>
2023-04-07 11:40:16 -0700

    fullscreen setting works (only in build mode)

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -24,4 +24,21 @@ public class SettingsController : MonoBehaviour
     {
         settingsMenu.SetActive(false);
     }
+
+    public void ToggleFullscreen(){
+        if (Screen.fullScreen == true)
+        {
+            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, false);
+        }
+        else
+        {
+            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
+        }
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = Screen.resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 }
