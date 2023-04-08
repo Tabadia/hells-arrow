2231d1db0441f3e37d582296fd916146e9804b71
Thalen <tabadia7574@student.pps.net>
2023-04-08 11:57:25 -0700

    Resolution setting (settings now up to OGCP standards)

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -3,12 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
 
 public class SettingsController : MonoBehaviour
 {
     [SerializeField] private GameObject settingsMenu;
     [SerializeField] private Button exitButton;
     [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    void Start() {
+        // Add good resolution options to the dropdown
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        
+        List<string> resOptions = new List<string>();
+        
+        int currentRes = 0;
+
+        for (int i = 0; i < resolutions.Length; i++) {
+            string res = resolutions[i].width + " x " + resolutions[i].height;
+            if (!resOptions.Contains(res)){
+                resOptions.Add(res);
+            }
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height) {
+                currentRes = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(resOptions);
+        resolutionDropdown.value = currentRes;
+        resolutionDropdown.RefreshShownValue();
+    }
+    
+    // public functions that are called by buttons in the settings menu
 
     public void SetVolume (float volume)
     {
@@ -36,9 +66,9 @@ public class SettingsController : MonoBehaviour
         }
     }
 
-    public void SetResolution(int resolutionIndex)
+    public void SetResolution(int resIndex)
     {
-        Resolution resolution = Screen.resolutions[resolutionIndex];
+        Resolution resolution = resolutions[resIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
